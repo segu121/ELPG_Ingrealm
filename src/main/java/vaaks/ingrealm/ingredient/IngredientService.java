59ed672b8e564bfedb6cf01f5a3a46e3a05e6642
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2021-11-18 21:20:58 +0100

    unfinished Ingredient module

---
+++ src/main/java/vaaks/ingrealm/ingredient/IngredientService.java
@@ -0,0 +1,40 @@
+package vaaks.ingrealm.ingredient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IngredientService {
+
+    private final IngredientRepository repository;
+
+    @Autowired
+    public IngredientService(IngredientRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Ingredient> getAll() {
+        return repository.findAll();
+    }
+
+    public Ingredient get(long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public Ingredient create(Ingredient ingredient) {
+        return repository.save(ingredient);
+    }
+
+    public Ingredient update(long id, Ingredient ingredient) {
+        Ingredient ingredientFromDb = get(id);
+        ingredientFromDb.setName(ingredient.getName());
+        ingredientFromDb.setRating(ingredient.getRating());
+        ingredientFromDb.setDescription(ingredient.getDescription());
+        return repository.save(ingredientFromDb);
+    }
+
+    public void delete(long id) {repository.delete(get(id));}
+
+}
