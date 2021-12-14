774f13daa8ae0bfd14eaf3c0d4adfc4e5f41e878
segu121 <slowik.sebastian1@gmail.com>
2021-12-14 20:59:21 +0100

    register, login validation, spring security

---
+++ src/main/java/vaaks/ingrealm/registration/token/ConfirmationToken.java
@@ -0,0 +1,50 @@
+package vaaks.ingrealm.registration.token;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+import vaaks.ingrealm.appuser.Users;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+    private Long id;
+    private String token;
+    private LocalDateTime createAt;
+    private LocalDateTime expiredAt;
+
+    private LocalDateTime confirmed;
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "users_id"
+    )
+    private Users user;
+
+    public ConfirmationToken(String token,
+                             LocalDateTime createAt,
+                             LocalDateTime expiredAt,
+                             Users user) {
+        this.token = token;
+        this.createAt = createAt;
+        this.expiredAt = expiredAt;
+    }
+}
